c0d9f60a115fc45a9c5decda2ee9d860c36feaa5
CUCUTRASCA <71490744+CUCUTRASCA@users.noreply.github.com>
2021-03-28 12:28:12 +0200

    Update TerminacionMasRepetida.java

--- src/TerminacionMasRepetida.java
+++ src/TerminacionMasRepetida.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TerminacionMasRepetida {
+	
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		try {
